6b190ab21e61cd82db4a4e5ab491ca0552eba760
Mytlogos <mytlogos@hotmail.de>
2021-03-02 22:28:39 +0100

    fix: fix model fields, change serializers base

--- todoya/models.py
+++ todoya/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 
+
 class Project(models.Model):
-    title: models.TextField()
-    parent: models.ForeignKey("Project", null=True, on_delete=models.CASCADE)
+    title = models.TextField()
+    parent = models.ForeignKey("Project", null=True, on_delete=models.CASCADE)
+
 
 class Board(models.Model):
-    title: models.TextField()
-    project: models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
 
 class Task(models.Model):
-    title: models.TextField()
-    start: models.DateTimeField()
-    due: models.DateTimeField()
-    status: models.FloatField(default=0)
-    completion_date: models.DateTimeField()
-    location: models.TextField()
-    description: models.TextField()
-    board: models.ForeignKey(Board, on_delete=models.CASCADE)
-    project: models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.TextField()
+    start = models.DateTimeField(null=True)
+    due = models.DateTimeField(null=True)
+    status = models.FloatField(default=0)
+    completion_date = models.DateTimeField(null=True)
+    location = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    board = models.ForeignKey(Board, null=True, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
 
 class Category(models.Model):
-    title: models.TextField()
-    tasks: models.ManyToManyField(Task)
+    title = models.TextField()
+    tasks = models.ManyToManyField(Task)
--- todoya/serializers.py
+++ todoya/serializers.py
@@ -2,23 +2,23 @@ from .models import Board, Task, Category, Project
 from rest_framework import serializers
 
 
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
 
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
-class BoardSerializer(serializers.HyperlinkedModelSerializer):
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = '__all__'
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
