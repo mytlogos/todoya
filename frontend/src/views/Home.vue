fa8ccb9af4b71d1d9ce1ec1c7562a38d150d326c
Mytlogos <mytlogos@hotmail.de>
2021-03-14 16:24:01 +0100

    feat: display task count in sidebar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,25 +4,42 @@
             <div class="m-2">Views</div>
             <ul class="list-group list-group-flush">
                 <router-link
+                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
                     :to="{ name: 'ListView', query: { view: 0 } }"
                     tag="a"
-                    >Today</router-link
-                >
+                    >Today
+                    <span class="badge badge-primary badge-pill">{{
+                        todayCount
+                    }}</span>
+                </router-link>
                 <router-link
+                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
                     :to="{ name: 'ListView', query: { view: 1 } }"
                     tag="a"
-                    >Tomorrow</router-link
-                >
+                    >Tomorrow
+                    <span class="badge badge-primary badge-pill">{{
+                        tomorrowCount
+                    }}</span>
+                </router-link>
                 <router-link
+                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
                     :to="{ name: 'ListView', query: { view: 2 } }"
                     tag="a"
-                    >Next Week</router-link
-                >
+                    >Next Week
+                    <span class="badge badge-primary badge-pill">{{
+                        nextWeekCount
+                    }}</span>
+                </router-link>
                 <router-link
+                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
                     :to="{ name: 'ListView', query: { view: 3 } }"
                     tag="a"
-                    >All</router-link
                 >
+                    All
+                    <span class="badge badge-primary badge-pill">{{
+                        allCount
+                    }}</span>
+                </router-link>
                 <router-link
                     class="list-group-item list-group-item-action"
                     :to="{ name: 'Calendar' }"
@@ -60,7 +77,7 @@
 </template>
 
 <script lang="ts">
-import { Project } from "@/client";
+import { Project, Task } from "@/client";
 import { defineComponent } from "vue";
 import { mapState } from "vuex";
 
@@ -68,15 +85,81 @@ export default defineComponent({
     name: "Home",
     data() {
         return {
-            selectedView: ""
+            selectedView: "",
+            todayCount: 0,
+            tomorrowCount: 0,
+            nextWeekCount: 0,
+            allCount: 0
         };
     },
     computed: {
-        ...mapState(["projects", "selectedProjects"])
+        ...mapState(["projects", "selectedProjects", "tasks"])
+    },
+    watch: {
+        tasks(tasks: Task[]) {
+            // after midnight today
+            const afterToday = new Date();
+            afterToday.setHours(0, 0, 0, 0);
+
+            // before midnight tomorrow
+            const beforeToday = new Date(afterToday);
+            beforeToday.setDate(beforeToday.getDate() + 1);
+            // after midnight tomorrow
+            const afterTomorrow = new Date();
+            afterTomorrow.setDate(afterTomorrow.getDate() + 1);
+            afterTomorrow.setHours(0, 0, 0, 0);
+
+            // before midnight the day after tomorrow
+            const beforeTomorrow = new Date(afterTomorrow);
+            beforeTomorrow.setDate(beforeTomorrow.getDate() + 1);
+            // after midnight today
+            const afterNextWeek = new Date();
+            afterNextWeek.setHours(0, 0, 0, 0);
+
+            // before midnight in seven days
+            const beforeNextWeek = new Date(afterNextWeek);
+            beforeNextWeek.setDate(beforeNextWeek.getDate() + 7);
+
+            this.allCount = tasks.length;
+            let todayCount = 0;
+            let tomorrowCount = 0;
+            let nextWeekCount = 0;
+
+            tasks.forEach(task => {
+                if (this.withinTimeBoundary(afterToday, beforeToday, task)) {
+                    todayCount++;
+                    nextWeekCount++;
+                } else if (
+                    this.withinTimeBoundary(afterTomorrow, beforeTomorrow, task)
+                ) {
+                    tomorrowCount++;
+                    nextWeekCount++;
+                } else if (
+                    this.withinTimeBoundary(afterNextWeek, beforeNextWeek, task)
+                ) {
+                    nextWeekCount++;
+                }
+            });
+
+            this.todayCount = todayCount;
+            this.tomorrowCount = tomorrowCount;
+            this.nextWeekCount = nextWeekCount;
+        }
     },
     methods: {
         toggle(project: Project, event: MouseEvent) {
-            this.$store.commit("toggleProjectSelect", {project, multi: event.ctrlKey});
+            this.$store.commit("toggleProjectSelect", {
+                project,
+                multi: event.ctrlKey
+            });
+        },
+        withinTimeBoundary(after: Date, before: Date, task: Task) {
+            return (
+                ((task.start && task.start > after) ||
+                    (task.due && task.due > after)) &&
+                ((task.start && task.start < before) ||
+                    (task.due && task.due < before))
+            );
         }
     }
 });
@@ -87,6 +170,8 @@ export default defineComponent({
     width: 10vw;
     overflow-y: auto;
     min-width: 10em;
-    height: calc(100vh - 60px); /* take 100% of viewport height minus the nav header */
+    height: calc(
+        100vh - 60px
+    ); /* take 100% of viewport height minus the nav header */
 }
 </style>
